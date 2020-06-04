4a13149357ac96168542c4c6ef459e3c09a55898
chiragbosco123 <66154979+chiragbosco123@users.noreply.github.com>
2020-06-04 18:48:51 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This two functions will help in caching the inverse of the matrix. Very long vector may take
+## too long to compute the mean. Thus below two functions helps in
+## storing the matrix and caches its inverse, thus saving the time 
+## This programming is done using the Laxical scoping  
 
-## Write a short comment describing this function
+
+## Below mentioned function is creating a matrix that helps in caching its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## Below function computes the inverse of the special matrix which was created by makeCacheMatrix.
+## if the inverse has been computed it will retrive the value from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+  
+  }
+
